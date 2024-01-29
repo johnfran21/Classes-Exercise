554c717798e582dccdeb7610fd7ec85a9bf15bff
John Franzese <johnfranzese21@gmail.com>
2024-01-28 21:31:17 -0600

    ClassesExercise

---
+++ Classes/CarClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Classes
+{
+	public class CarClass
+	{
+		public CarClass()
+		{
+		}
+	}
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var future = new Car();
+
+            future.Make = "BMW";
+            future.Model = "Sedan";
+            future.Year = 2035;
+
+            Console.WriteLine($"This is a {future.Make} from the future. " +
+                $"Its a four door {future.Model} " +
+                $"and will come out in the year {future.Year}!");
         }
     }
 }
